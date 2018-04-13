34d3e070bb0537bcea4446aad8f04a440763fc71
Taylor D. Edmiston <tedmiston@gmail.com>
2018-04-13 00:22:50 -0400

    Use pytest

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,39 +1,31 @@
-import unittest
-
 from mailgun_plugin.utils import is_likely_human
 
 
-class IsLikelyHumanTestCase(unittest.TestCase):
+def test_is_likely_human_valid():
+    response = {
+        'address': 'taylor@astronomer.io',
+        'is_disposable_address': False,
+        'is_role_address': False,
+        'is_valid': True,
+    }
+    assert is_likely_human(response) is True
 
-    def test_is_likely_human_valid(self):
-        expected = True
-        response = {
-            'address': 'taylor@astronomer.io',
-            'is_disposable_address': False,
-            'is_role_address': False,
-            'is_valid': True,
-        }
-        actual = is_likely_human(response)
-        self.assertEqual(expected, actual)
 
-    def test_is_likely_human_invalid(self):
-        expected = False
-        response = {
-            'address': 'mark.zuckerberg@astronomer.io',
-            'is_disposable_address': False,
-            'is_role_address': False,
-            'is_valid': False,
-        }
-        actual = is_likely_human(response)
-        self.assertEqual(expected, actual)
+def test_is_likely_human_invalid():
+    response = {
+        'address': 'mark.zuckerberg@astronomer.io',
+        'is_disposable_address': False,
+        'is_role_address': False,
+        'is_valid': False,
+    }
+    assert is_likely_human(response) is False
 
-    def test_is_likely_human_role(self):
-        expected = False
-        response = {
-            'address': 'info@astronomer.io',
-            'is_disposable_address': False,
-            'is_role_address': True,
-            'is_valid': True,
-        }
-        actual = is_likely_human(response)
-        self.assertEqual(expected, actual)
+
+def test_is_likely_human_role():
+    response = {
+        'address': 'info@astronomer.io',
+        'is_disposable_address': False,
+        'is_role_address': True,
+        'is_valid': True,
+    }
+    assert is_likely_human(response) is False
