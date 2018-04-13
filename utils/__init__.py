f924ee0b3d7b437496ec7973bf66d138ae18bfa5
Taylor D. Edmiston <tedmiston@gmail.com>
2018-04-12 21:16:07 -0400

    Rename list parser class

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
+from .contact_list_parser import parse_ndjson_from_contents
 from .email_parser import filter_address_only
 from .json_datetime_serializer import json_serial
 from .mailgun import is_likely_human
-from .pardot_parser import parse_ndjson_from_contents
