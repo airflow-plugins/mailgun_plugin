b08b740e7f16eb069f80de99339d393c099d9a8e
Taylor D. Edmiston <tedmiston@gmail.com>
2018-04-16 10:43:06 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='airflow_mailgun_plugin',
-    version='0.0.0',
+    version='0.0.1',
     description='A hook and operator for the Mailgun Email Validation API',
     long_description=open('README.rst').read(),
     url='https://github.com/airflow-plugins/mailgun_plugin',
